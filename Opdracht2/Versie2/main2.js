d4aa76be16e9ee2eccf72956a2c51bd3af26f389
KayleeMH <kayleemh@live.nl>
2018-03-18 13:57:23 +0100

    dots added

--- Opdracht2/Versie2/main2.js
+++ Opdracht2/Versie2/main2.js
@@ -7,85 +7,67 @@ var prevbutton = document.querySelector("button:nth-child(1)");
 /* items opslaan */
 var box = document.querySelector('.container');
 var items = box.querySelectorAll('.content li');
+var box2 = document.querySelector('.dotcontainer');
+var dots = box2.querySelectorAll('.transparentdot');
 
 /* counter om nummer in op te slaan op welke slide actief is */
 var counter = 0;
+var dotcounter = 0;
 
 /* om te tellen hoeveel list items er zijn */
 var alleslides = items.length;
+var alledots = dots.length;
 
 /* welke slide is actief */
 var currentslide = items[0];
+var currentdot = dots[0];
+console.log(currentdot);
 console.log(currentslide);
 
 var nextslide = function () {
-    currentslide.classList.remove('mainslide');
-    currentslide.classList.add('slides');
+    currentslide.classList.remove('active');
+    currentdot.classList.remove('activedot');
     /* counter gaat een omhoog door klikken van next */
-    counter = counter + 1;
-    if (counter >= 4) {
+    counter = counter + 1; //counter++ zou ook moten werken
+    dotcounter = dotcounter + 1;
+    if (counter >= 4 && dotcounter >= 4) {
         counter = 0;
+        dotcounter = 0;
     }
-    console.log(counter);
-    /* nieuwe currentslide is nieuwe getal van counter */
     currentslide = items[counter];
-    currentslide.classList.remove('slides');
-    currentslide.classList.add('mainslide');
+    currentdot = dots[dotcounter];
+    currentslide.classList.add('active');
+    currentdot.classList.add('activedot');
+    console.log(counter);
 }
 nextbutton.addEventListener('click', nextslide);
 
-function nextslidekey(event) {
-    var checkkey = event.keyCode;
-    if (checkkey == 39) {
-        console.log("riGHT ARROW PRESSED!");
-        currentslide.classList.remove('mainslide');
-        currentslide.classList.add('slides');
-        /* counter gaat een omhoog door klikken van next */
-        counter = counter + 1;
-        if (counter >= 4) {
-            counter = 0;
-        }
-        console.log(counter);
-        /* nieuwe currentslide is nieuwe getal van counter */
-        currentslide = items[counter];
-        currentslide.classList.remove('slides');
-        currentslide.classList.add('mainslide');
-    }
-}
-document.addEventListener('keydown', nextslidekey);
-
 var lastslide = function () {
-    currentslide.classList.remove('mainslide');
-    currentslide.classList.add('slides');
-    /* counter gaat een omhoog door klikken van next */
+    /* counter gaat een omlaag door klikken van prev */
+    currentslide.classList.remove('active');
+    currentdot.classList.remove('activedot');
     counter = counter - 1;
-    if (counter <= -1) {
+    dotcounter = dotcounter -1;
+    if (counter <= -1 && dotcounter <= -1) {
         counter = counter + 4;
+        dotcounter = dotcounter + 4;
     }
-    console.log(counter);
-    /* nieuwe currentslide is nieuwe getal van counter */
     currentslide = items[counter];
-    currentslide.classList.remove('slides');
-    currentslide.classList.add('mainslide');
+    currentdot = dots[dotcounter];
+    currentslide.classList.add('active');
+    currentdot.classList.add('activedot');
+    console.log(counter);
 }
 prevbutton.addEventListener("click", lastslide)
 
-function lastslidekey(event) {
+function slidekey(event) {
     var checkkey = event.keyCode;
+    if (checkkey == 39) {
+        nextslide();
+    }
     if (checkkey == 37) {
-        console.log("Left arrow is pressed");
-        currentslide.classList.remove('mainslide');
-        currentslide.classList.add('slides');
-        /* counter gaat een omhoog door klikken van next */
-        counter = counter - 1;
-        if (counter <= -1) {
-            counter = counter + 4;
-        }
-        console.log(counter);
-        /* nieuwe currentslide is nieuwe getal van counter */
-        currentslide = items[counter];
-        currentslide.classList.remove('slides');
-        currentslide.classList.add('mainslide');
+        lastslide();
     }
 }
-document.addEventListener('keydown', lastslidekey);
+
+document.addEventListener('keydown', slidekey);
