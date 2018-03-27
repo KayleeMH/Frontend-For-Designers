543ff1050e710fbade09eede8948e45fb8a65287
KayleeMH <kayleemh@live.nl>
2018-03-27 17:00:57 +0200

    start json

--- Opdracht3/Versie1/main.js
+++ Opdracht3/Versie1/main.js
@@ -1,4 +1,6 @@
 console.log('test');
+var header = document.querySelector('header');
+console.log(header);
 
 var requestURL = 'http://dennistel.nl/movies';
 console.log(requestURL);
@@ -7,13 +9,21 @@ var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
+
 request.onload = function () {
     var jasonfile = request.response;
     console.log(jasonfile);
+    maakh1(jasonfile);
+
 }
 
-function populateHeader(jsonObj) {
+var maakh1 = function (jsonObj) {
     var myH1 = document.createElement('h1');
-    myH1.textContent = jsonObj['squadName'];
+    myH1.textContent = jsonObj[0]['title'];
     header.appendChild(myH1);
+    console.log(jsonObj[0]['title']);
 }
+
+var films = jsonObj;
+console.log(films);
+//for (var i = 0; i < films.length; i++) {
